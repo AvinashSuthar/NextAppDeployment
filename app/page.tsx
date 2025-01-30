3b7f8542962e17c91783d695cfce0433651556ee
AVINASH SUTHAR <avinashsuthar19hmh@gmail.com>
2025-01-30 14:26:11 +0530

    avinahs

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Link from "next/link";
 export default function page() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Hello World this is my first next applicatian
+      this  is Avinash Suthar 
+      
       <Link href="/home">Go to home page</Link>
     </div>
   );
